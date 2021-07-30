c029ca7f540aee3f875f540428fdfc47f56bed47
RachmanForniandi <rachmanforniandi@gmail.com>
2021-07-30 21:13:44 +0700

    added model paging resource done.

--- app/src/main/java/com/richarddewan/paging3_todo/model/TaskResponse.kt
+++ app/src/main/java/com/richarddewan/paging3_todo/model/TaskResponse.kt
@@ -31,4 +31,24 @@ data class TaskResponse(
     val to: Int,
     @SerializedName("total")
     val total: Int
-)
+){
+    @Keep
+    data class Task(
+        @SerializedName("id")
+        val id: String,
+        @SerializedName("user_id")
+        val userId: String,
+        @SerializedName("title")
+        val title: String,
+        @SerializedName("body")
+        val body: String,
+        @SerializedName("note")
+        val note: String,
+        @SerializedName("status")
+        val status: String,
+        @SerializedName("created_at")
+        val createdAt: String,
+        @SerializedName("updated_at")
+        val updatedAt: String
+    )
+}
