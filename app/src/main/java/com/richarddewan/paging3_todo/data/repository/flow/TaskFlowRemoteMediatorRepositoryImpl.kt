77a51caf9f3a43d2e2b7f1c9a56f582a36e9d898
RachmanForniandi <rachmanforniandi@gmail.com>
2021-12-09 08:08:55 +0700

    task flow remote mediator repository done

--- app/src/main/java/com/richarddewan/paging3_todo/data/repository/flow/TaskFlowRemoteMediatorRepositoryImpl.kt
+++ app/src/main/java/com/richarddewan/paging3_todo/data/repository/flow/TaskFlowRemoteMediatorRepositoryImpl.kt
@@ -1,16 +1,36 @@
 package com.richarddewan.paging3_todo.data.repository.flow
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.richarddewan.paging3_todo.data.local.db.DbService
 import com.richarddewan.paging3_todo.data.local.entity.TaskEntity
+import com.richarddewan.paging3_todo.data.repository.paging.TaskFlowRemoteMediator
 import kotlinx.coroutines.flow.Flow
 
-class TaskFlowRemoteMediatorRepositoryImpl(
+class TaskFlowRemoteMediatorRepositoryImpl @ExperimentalPagingApi constructor(
     private val dbService:DbService,
-private val remoteMediatorRepositoryImpl: TaskFlowRemoteMediatorRepositoryImpl):TaskFlowRemoteMediatorRepository {
+private val taskFlowRemoteMediator: TaskFlowRemoteMediator):TaskFlowRemoteMediatorRepository {
 
+
+    @ExperimentalPagingApi
     override fun getTaskList(): Flow<PagingData<TaskEntity>> {
-        TODO("Not yet implemented")
+        return Pager(
+            config = defaultPagingConfig(),
+            remoteMediator = taskFlowRemoteMediator,
+            pagingSourceFactory = {dbService.taskFlowDao().getTasks()}
+        ).flow
+    }
+
+    private fun defaultPagingConfig(): PagingConfig {
+        return PagingConfig(
+            pageSize = 10,
+            prefetchDistance = 10,
+            enablePlaceholders = false,
+            initialLoadSize = 20,
+            maxSize = 30 //pageSize + (2 * prefetchDistance)`) of the most recent load.
+        )
     }
 
 }
