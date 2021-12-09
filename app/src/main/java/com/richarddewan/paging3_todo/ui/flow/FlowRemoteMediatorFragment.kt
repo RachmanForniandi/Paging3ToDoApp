f7ad566993ddc3b06f6a448dc69e195bd8ccd63a
RachmanForniandi <rachmanforniandi@gmail.com>
2021-12-09 15:19:25 +0700

    implemented Flow remote mediator viewmodel done

--- app/src/main/java/com/richarddewan/paging3_todo/ui/flow/FlowRemoteMediatorFragment.kt
+++ app/src/main/java/com/richarddewan/paging3_todo/ui/flow/FlowRemoteMediatorFragment.kt
@@ -5,8 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.ExperimentalPagingApi
+import com.richarddewan.paging3_todo.data.repository.flow.TaskFlowRemoteMediatorRepositoryImpl
+import com.richarddewan.paging3_todo.data.repository.paging.TaskFlowRemoteMediator
 import com.richarddewan.paging3_todo.databinding.FragmentFlowPagingSourceBinding
 import com.richarddewan.paging3_todo.databinding.FragmentFlowRemoteMediatorBinding
+import com.richarddewan.paging3_todo.ui.flow.viewmodel.FlowRemoteMediatorViewModel
+import com.richarddewan.paging3_todo.util.MyApplication
+import com.richarddewan.paging3_todo.util.ViewModelProviderFactory
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 /*
@@ -15,17 +25,42 @@ created by Richard Dewan 11/04/2021
 
 class FlowRemoteMediatorFragment: Fragment() {
     private lateinit var binding: FragmentFlowRemoteMediatorBinding
+    private lateinit var viewModel: FlowRemoteMediatorViewModel
+    private lateinit var repository: TaskFlowRemoteMediatorRepositoryImpl
 
+    @ExperimentalPagingApi
+    private lateinit var remoteMediator:TaskFlowRemoteMediator
+
+    @ExperimentalPagingApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentFlowRemoteMediatorBinding.inflate(layoutInflater)
+
+        val netService = (requireActivity().application as MyApplication).networkService
+        val dbService = (requireActivity().application as MyApplication).dbService
+
+        remoteMediator = TaskFlowRemoteMediator(netService,dbService)
+        repository = TaskFlowRemoteMediatorRepositoryImpl(dbService,remoteMediator)
+
+        viewModel = ViewModelProvider(this, ViewModelProviderFactory(FlowRemoteMediatorViewModel::class){
+            FlowRemoteMediatorViewModel(repository)
+        }).get(FlowRemoteMediatorViewModel::class.java)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    @ExperimentalPagingApi
+    private fun observers(){
+        lifecycleScope.launch {
+            viewModel.getTaskList()
+                .collectLatest {  }
+        }
     }
 }
